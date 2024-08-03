3bc8a2cfe8bdf5de9b51aaff81a65e3f000c3d32
John Siordia <siohn@proton.me>
2024-08-03 16:08:44 -0500

    Project 9 - in progress

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,9 @@
 //personal token: b7515436-5dfa-4f13-a09a-5183c1df5fb3
 
 class Api {
-  constructor(options) {}
+  constructor() {
+    this._baseUrl = "https://around-api.en.tripleten-services.com/v1";
+  }
 
   getUserData() {
     return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
